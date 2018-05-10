0ab08aa6a5cf019df579934afeacee57b9f3f51d
Jesse <laethaka@gmail.com>
2018-05-10 13:52:01 -0700

    map kinda inits

---
+++ assets/javascript/app.js
@@ -0,0 +1,37 @@
+var map;
+var infowindow;
+function initMap() {
+  var city = {lat: -33.867, lng: 151.195};
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+
+infowindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch({
+    location: city,
+    radius: 100,
+    type: ['store']
+  }, callback);
+}
+
+function callback(results, status) {
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+}
+
+var mapInput1 = $('#cityInput1').val();
+
+// console.log(https://maps.goo)
