577b3e726af23b5d2ed4caf51b356eb69853a8b8
Jesse <laethaka@gmail.com>
2018-05-12 10:11:27 -0700

    changing to desktop

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -37,43 +37,27 @@ function initMap(centerLat, centerLng) {
     }).then (function(response){
         var markerArr = [];
         for (let idx=0; idx<response.results.length; idx++){
-            console.log(response.results[idx])
+            // console.log(response.results[idx])
             var markerLatLng = {lat: response.results[idx].geometry.location.lat, lng: response.results[idx].geometry.location.lng};
             markerArr[idx] = new google.maps.Marker({
                 position: markerLatLng,
                 map: map,
                 title: response.results[idx].name
             });
+            var cardDetailIdx = cardIdx;
             markerArr[idx].addListener('click', function() {
-                console.log(`you clicked ${response.results[idx].name}`)
+                // console.log(`you clicked ${response.results[idx].name}`)
+                console.log(cardDetailIdx)
+                // $(`#accommodation-name${cardIdx-1}`).text(response.results[idx].name)
             });
         };
     });
 };
 
-// function placeMarkers(myLat, myLng) {
-//     $.ajax({
-//         url: `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyBEL_ixBbgLQWdqBAVuH5Ibs-WTuYdjhqo&radius=1500&keyword=hotel&location=${myLat},${myLng}`,
-//         method: 'GET'
-//     }).then (function(response){
-//         var markerArr = [];
-//         for (let idx=0; idx<response.results.length; idx++){
-//             console.log(response.results[idx])
-//             var markerLatLng = {lat: response.results[idx].geometry.location.lat, lng: response.results[idx].geometry.location.lng};
-
-//             markerArr[idx] = new google.maps.Marker({
-//                 position: markerLatLng,
-//                 map: map,
-//                 title: 'A hotel!'
-//               });
-//         }
-//     })
-// }
-
 // Dynamically creating a new card function
 function generateCard(city) {
     var newCard = $(`<div id="card1" class="col-md-4"><div class="card"><h5 id="city-name">${city}</h5><div class='map' id="newMap"></div><div class="card-info-box">
-    <h4 id="accommodation-name${cardIdx}">${city}</h4>
+    <h4 id="accommodation-name${cardIdx}"></h4>
     <p>Price <span id="price-id${cardIdx}"></span> </p><p>Rating <span id="rating-id${cardIdx}"></span></p><button href="#" class="btn btn-outline-dark">More Info</button>
     </div><div id="carouselExampleControls${cardIdx}" class="carousel slide" data-ride="carousel"><div class="carousel-inner"><div class="carousel-item active"><img id="img-accommodation-1" class="d-block w-100" src="assets/img/hotel-1.jpeg" alt="First slide"></div><div class="carousel-item"><img id="img-accommodation-2" class="d-block w-100" src="assets/img/hotel-2.jpeg" alt="Second slide"></div><div class="carousel-item"><img id="img-accommodation-3" class="d-block w-100" src="assets/img/hotel-3.jpeg" alt="Third slide"></div></div><a class="carousel-control-prev" href="#carouselExampleControls${cardIdx}" role="button" data-slide="prev"><span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="sr-only">Previous</span></a><a class="carousel-control-next" href="#carouselExampleControls${cardIdx}" role="button" data-slide="next"><span class="carousel-control-next-icon" aria-hidden="true"></span><span class="sr-only">Next</span></a></div></div></div>`);
     
